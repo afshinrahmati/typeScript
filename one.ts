ccf377ed39e14c8d4b34533d0fcd9534c4d47040
afshinrahmati <afshinrahmati93@gmail.com>
2021-09-11 14:27:23 -0400

    + and ! and as HTMLINPUT

--- one.ts
+++ one.ts
@@ -1,9 +1,12 @@
 var button = document.querySelector("button");
-var input1 = document.getElementById("num1");
-var input2 = document.getElementById("num2");
-function add(num1, num2) {
+//!==> never null
+// as HTMLInputElement ==> type innerHtml
+var input1 = document.getElementById("num1")! as HTMLInputElement;
+var input2 = document.getElementById("num2")! as HTMLInputElement;
+function add(num1: number, num2: number) {
     return num1 + num2;
 }
+// + ==> change string to number
 button.addEventListener("click", function () {
-    console.log(add(input1.value, input2.value));
+    console.log(add(+input1.value, +input2.value));
 });
